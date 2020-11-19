940450fd7d07e843430306a31629f5473877a29b
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-11-19 17:48:26 -0300

    Validação de email e senha implementados

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Login.css';
 
 function Login() {
-  const isDisabled = true;
+  const [localState, setLocalState] = useState({
+    email: '',
+    password: '',
+    isDisabled: true,
+  });
+  const { email, password, isDisabled } = localState;
+  const onChange = ({ target }) => {
+    const { value, name } = target;
+    setLocalState({
+      ...localState,
+      [name]: value,
+    });
+  };
+  const validateLoginInfo = () => {
+    const minPassChar = 6;
+    const emailExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const validated = password.length > minPassChar
+      && password.length !== 0
+      && emailExp.test(email);
+    if (validated) {
+      console.log('entrou');
+      setLocalState({
+        ...localState,
+        isDisabled: false,
+      });
+    } else {
+      console.log('entrou no else');
+      setLocalState({
+        ...localState,
+        isDisabled: true,
+      });
+    }
+  };
+  useEffect(() => {
+    validateLoginInfo();
+  }, [email, password, isDisabled]);
+
   return (
     <div className="Login-body">
       <form className="Login-form">
         <label>
           <input
-            type="text"
-            placeholder="E-mail"
-            id="email-input"
             data-testid="email-input"
+            id="email-input"
+            name="email"
+            onChange={onChange}
+            placeholder="E-mail"
+            type="text"
+            value={email}
           />
         </label>
         <label>
           <input
-            type="text"
-            id="password-input"
-            placeholder="Password"
             data-testid="password-input"
+            id="password-input"
+            name="password"
+            onChange={onChange}
+            placeholder="Password"
+            type="password"
+            value={password}
           />
         </label>
         <button
