e963e9137344e8afb6ad022ebc50e8145bea0097
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-11-23 22:08:17 -0300

    ESLint, linha em branco corrigida

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import DrinkList from '../components/DrinkList';
 
-
 function Bebidas() {
   const enableSearch = true;
   return (
