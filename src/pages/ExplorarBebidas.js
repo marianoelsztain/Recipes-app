4f5a07e772909f2ae5eb5681ca57cbbd3669316c
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-11-26 13:31:14 -0300

    Finalizando CSS Parcial

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ExploreDetailsDrinks from '../components/ExploreDetailsDrinks';
+import '../css/Explorar.css';
 
 function ExplorarBebidas() {
   return (
